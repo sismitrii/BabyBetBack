a88b622dbb88219d2b9913736697672fbbef5f69
Florian Guerin <53363911+sismitrii@users.noreply.github.com>
2025-01-14 12:11:35 +0100

    test deploy with db

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -149,6 +149,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<BetDbContext>();
+    db.Database.EnsureDeleted();
     db.Database.EnsureCreated(); // Crée la base si elle n'existe pas
     db.Database.Migrate(); // Applique les migrations si nécessaire
 }
