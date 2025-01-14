b81aec88d4abab8ff72677a0b8cbb6811f26b067
Florian Guerin <53363911+sismitrii@users.noreply.github.com>
2025-01-14 11:53:21 +0100

    test

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -150,7 +150,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<BetDbContext>();
     db.Database.EnsureCreated(); // Crée la base si elle n'existe pas
-    db.Database.Migrate(); // Applique les migrations si nécessaire
+    //db.Database.Migrate(); // Applique les migrations si nécessaire
 }
 app.Run();
 
