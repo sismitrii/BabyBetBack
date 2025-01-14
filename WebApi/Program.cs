286137ceb4613600e8eadd107efc8f8f07c0be2d
Florian Guerin <53363911+sismitrii@users.noreply.github.com>
2025-01-14 14:03:20 +0100

    test

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -149,7 +149,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<BetDbContext>();
-    db.Database.Migrate(); // Applique les migrations si nécessaire
+    //db.Database.Migrate(); // Applique les migrations si nécessaire
 }
 app.Run();
 
