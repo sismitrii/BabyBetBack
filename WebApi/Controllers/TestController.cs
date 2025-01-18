7b9a5453d872370423f28893c03c28f661744fef
Florian Guerin <53363911+sismitrii@users.noreply.github.com>
2025-01-18 10:10:53 +0100

    add possibility to replace a Db

--- WebApi/Controllers/TestController.cs
+++ WebApi/Controllers/TestController.cs
@@ -65,12 +65,39 @@ public class TestController(IBetService betService) :ControllerBase
         var sqliteDb = Path.Combine(_dataFolderPath, "sqlite.db");
 
         if (!System.IO.File.Exists(sqliteDb))
-            return NotFound("Le fichier de base de données n'existe pas.");
+            return NotFound("db file doesn't exist !");
 
         var fileBytes = System.IO.File.ReadAllBytes(sqliteDb);
         var fileName = "sqlite.db";
         return File(fileBytes, "application/octet-stream", fileName);
     }
     
-    
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadDatabase([FromForm] IFormFile file)
+    {
+        if (User.GetNameIdentifierId() != "floguerin156@gmail.com")
+            return Forbid();
+        
+        var sqliteDb = Path.Combine(_dataFolderPath, "sqlite_test.db");
+        
+        if (file == null || file.Length == 0)
+            return BadRequest("Please provide a file.");
+
+        if (!file.FileName.EndsWith(".db", StringComparison.OrdinalIgnoreCase))
+            return BadRequest("File must be a Sqlite Db (.db).");
+
+        var tempPath = Path.Combine(Path.GetTempPath(), file.FileName);
+
+        await using (var stream = new FileStream(tempPath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        if (System.IO.File.Exists(sqliteDb))
+            System.IO.File.Delete(sqliteDb);
+
+        System.IO.File.Move(tempPath, sqliteDb);
+
+        return Ok("Base db replace successfully");
+    }
 }
